829a8802a0f99eeb492eb4c21ec654379acdea61
slpuk <yarik6052@gmail.com>
2026-04-03 13:42:55 +0300

    chore: raised version to 0.6.0-alpha.7

--- loggity/__init__.py
+++ loggity/__init__.py
@@ -1,5 +1,5 @@
 from .logger import Logger, Colors, LoggerConfig
 
-__version__ = "0.5.0-alpha.6"
+__version__ = "0.6.0-alpha.7"
 __author__ = "slpuk"
 __all__ = ["Logger", "Colors", "LoggerConfig"]
